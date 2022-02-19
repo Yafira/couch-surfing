012bc192b4dba582e3ea7d7188e232ab4636624e
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-19 18:38:18 -0500

    fixed function to show price/night for each property using union types

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { showReviewTotal, populateUser } from './utils.js';
 import { Permissions, LoyaltyUser } from './enums.js';
 const propertyContainer = document.querySelector('.properties');
 const footer = document.querySelector('.footer');
-let isOpen;
+let isLoggedIn;
 // Reviews
 const reviews = [
     {
@@ -79,6 +79,15 @@ const properties = [
 // Functions
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 populateUser(you.isReturning, you.firstName);
+let authorityStatus;
+isLoggedIn = false;
+function showDetails(authorityStatus, element, price) {
+    if (authorityStatus) {
+        const priceDisplay = document.createElement('div');
+        priceDisplay.innerHTML = price.toString() + '/night';
+        element.appendChild(priceDisplay);
+    }
+}
 // Add the properties
 for (let i = 0; i < properties.length; i++) {
     const card = document.createElement('div');
@@ -88,6 +97,7 @@ for (let i = 0; i < properties.length; i++) {
     image.setAttribute('src', properties[i].image);
     card.appendChild(image);
     propertyContainer === null || propertyContainer === void 0 ? void 0 : propertyContainer.appendChild(card);
+    showDetails(you.permissions, card, properties[i].price);
 }
 // Use your current location, time and temperature
 let currentLocation = ['New York', '0:40', 26];
--- script.ts
+++ script.ts
@@ -3,7 +3,7 @@ import { Permissions, LoyaltyUser } from './enums.js';
 const propertyContainer = document.querySelector('.properties');
 const footer = document.querySelector('.footer');
 
-let isOpen: boolean;
+let isLoggedIn: boolean;
 
 // Reviews
 const reviews: any[] = [
@@ -95,8 +95,23 @@ const properties: {
 
 // Functions
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
-
 populateUser(you.isReturning, you.firstName);
+
+let authorityStatus: any;
+
+isLoggedIn = false;
+
+function showDetails(
+	authorityStatus: boolean | Permissions,
+	element: HTMLDivElement,
+	price: number
+) {
+	if (authorityStatus) {
+		const priceDisplay = document.createElement('div');
+		priceDisplay.innerHTML = price.toString() + '/night';
+		element.appendChild(priceDisplay);
+	}
+}
 
 // Add the properties
 for (let i = 0; i < properties.length; i++) {
@@ -107,6 +122,7 @@ for (let i = 0; i < properties.length; i++) {
 	image.setAttribute('src', properties[i].image);
 	card.appendChild(image);
 	propertyContainer?.appendChild(card);
+	showDetails(you.permissions, card, properties[i].price);
 }
 
 // Use your current location, time and temperature
