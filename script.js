36913096f9ecadb90d5da86c0ae29732e97171cd
Yafira <yafira.martinez@lc.cuny.edu>
2022-02-19 16:14:29 -0500

    implemented enum types

---
+++ script.js
@@ -0,0 +1,99 @@
+"use strict";
+exports.__esModule = true;
+var propertyContainer = document.querySelector('.properties');
+var footer = document.querySelector('.footer');
+var utils_1 = require("./utils");
+var isOpen;
+// Reviews
+var reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+];
+// User
+var you = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+};
+// Properties
+var properties = [
+    {
+        image: '',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: [+1123495082908, 'marywinkle@gmail.com'],
+        isAvailable: true
+    },
+    {
+        image: '',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: [+1123495082908, 'garydavis@hotmail.com'],
+        isAvailable: false
+    },
+    {
+        image: '',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom'
+        },
+        contact: [+1123495082908, 'andyluger@aol.com'],
+        isAvailable: true
+    },
+];
+// Functions
+(0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+(0, utils_1.populateUser)(you.isReturning, you.firstName);
+// Add the properties
+for (var i = 0; i < properties.length; i++) {
+    var card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = properties[i].title;
+    var image = document.createElement('img');
+    image.setAttribute('src', properties[i].image);
+    card.appendChild(image);
+    propertyContainer.appendChild(card);
+}
+// Use your current location, time and temperature
+var currentLocation = ['New York', '0:40', 26];
+footer.innerHTML =
+    currentLocation[0] +
+        ' ' +
+        currentLocation[1] +
+        ' ' +
+        currentLocation[2] +
+        '°';
